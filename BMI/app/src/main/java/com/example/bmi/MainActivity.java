b082f28a236fe5b3be7d13956c3f1aef136f0b66
A111222016 <A111222016@mail.shu.edu.tw>
2024-03-22 15:02:09 +0800

    2024_03_22

---
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txvshow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView txvshow = (TextView) findViewById(R.id.txvshow);
+        txvshow.setTextSize(36);
+        Button btnCalc = (Button) findViewById(R.id.btnCalc);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
+        btnCalc.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText.edtHeight = (EditText) findViewById(R.id.edtHeight);
+        EditText.edtWeight = (EditText) findViewById(R.id.edtWeight);
+        if(v.getId() == R.id.btnCalc){
+            double height = Double.parseDouble(edtHeight.geText().toString());
+            double weight = Double.parseDouble(edtWeight.geText().toString());
+            double bmi = weight / Math.pow(height/100.0,2);
+            if(bmi>=24)
+                txvshow.setTextColor(Color.RED);
+            else if(bmi<18.5)
+                txvshow.setTextColor(Color.BLUE);
+            else
+                txvshow.setTextColor(Color.GREEN);
+
+            txvshow.setText(String.format("%.2f"),bmi);
+        }
+        else {
+            edtHeight.setText("0");
+            edtWeight.setText("0");
+            txvshow.setText("");
+        }
+
+
+    }
+}
