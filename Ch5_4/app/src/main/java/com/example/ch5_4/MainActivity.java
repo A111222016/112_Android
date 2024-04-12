367a9feebb5139c99806387b6319b3faf38a8e06
A111222016 <A111222016@mail.shu.edu.tw>
2024-04-12 15:03:37 +0800

    2024_04_12

--- Ch5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
+++ Ch5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ch5_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            EditText txtInput = (EditText)findViewById(R.id.txtInput);
+            txtInput.setText("按下BACK按鍵");
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+    @Override
+    public boolean onKeyUp(int keyCode,KeyEvent event){
+        TextView output = (TextView) findViewById(R.id.lblOutput);
+        lbl.setText("按下"+keyCode);
+        return true;
+    }
+    return super.onKeyUp(keyCode,event);
 }
